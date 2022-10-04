43224e65d371ad792a5eed32c5b16e7e407fb761
Gerda522 <90824550+Gerda522@users.noreply.github.com>
2022-10-04 09:34:04 +0200

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,23 @@
+// Når websitet indlæses, cache ressourcer nævnt i listen
+const cacheName = "cache-insects";
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll([
+        "/insects/",
+        "/insects/index.html",
+        "/insects/butterflies.jpg",
+      ]);
+    })
+  );
+});
+
+// Hvis ressource ikke er tilgængelig online, så søg i cachen
+// efter et match.
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.open(cacheName).then((cache) => cache.match(event.request))
+    )
+  );
+});
